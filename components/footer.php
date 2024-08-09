d6ba54bfafb8aab201066c106723f4371fc9626f
smarthomes23 <smarthomesdesign23@gmail.com>
2024-08-09 18:24:48 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<div class="footer" style="background:#202020">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-xxl-3 col-xxl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <img class="white-filter" src="./images/logo.png" width="150"/>
+            </div>
+            <div class="col-xxl-3 col-xxl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <div class="p-3">
+                    <p class="text-white fs-3 vidaloka">Contact</p>
+                    <p class="text-secondary">1205 Ganesh Glory, Near BSNL Office, Jagatpur Chenpur Road, SG Highway Jagatpur, Ahmedabad - 382481 Gujarat, India </p>
+                    <a class="text-secondary">info@smart-homes.in</a>
+                    <p class="text-secondary pt-2">+91 7096961250</p>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-xxl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <div class="p-3">
+                    <p class="text-white fs-3 vidaloka">Useful links</p>
+                    <div class="menus">
+                        <a href="/" class="text-secondary text-decoration-none">About Us</a>
+                        <span></span>
+                        <a href="/" class="text-secondary text-decoration-none">Terms & Conditions</a>
+                        <span></span>
+                        <a href="/" class="text-secondary text-decoration-none">User Guide</a>
+                        <span></span>
+                        <a href="/" class="text-secondary text-decoration-none">Support Center</a>
+                        <span></span>
+                        <a href="/" class="text-secondary text-decoration-none">Privacy Policy</a>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-xxl-3 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="p-3">
+                    <p class="text-white fs-3 vidaloka">Newsletter</p>
+                    <div class="send-mail d-flex">
+                        <input type="text" placeholder="Email address.."/>
+                        <button><i class="bi bi-arrow-right-short"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Banner change background 
+document.addEventListener('DOMContentLoaded', function() {
+    const banner = document.querySelector('.banner');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    let backgroundToggle = true;
+    let interval;
+
+    // Function to toggle background
+    function toggleBackground() {
+        if (backgroundToggle) {
+            banner.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('./images/villa2.jpg')";
+        } else {
+            banner.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('./images/villa1.png')";
+        }
+        backgroundToggle = !backgroundToggle;
+        backgroundToggle?btn1.checked=true:btn2.checked=true;
+    }
+
+    // Automatic background change every 5 seconds
+    function startAutoToggle() {
+        interval = setInterval(toggleBackground, 5000);
+    }
+
+    // Manual control
+    btn1.addEventListener('click', function() {
+        clearInterval(interval); // Stop automatic toggle
+        banner.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('./images/villa1.png')";
+        backgroundToggle = false; // Reset toggle state
+        startAutoToggle(); // Restart automatic toggle
+    });
+
+    btn2.addEventListener('click', function() {
+        clearInterval(interval); // Stop automatic toggle
+        banner.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('./images/villa2.jpg')";
+        backgroundToggle = true; // Reset toggle state
+        startAutoToggle(); // Restart automatic toggle
+    });
+
+    // Start the automatic background change on load
+    startAutoToggle();
+});
